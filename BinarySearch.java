f8a5c038a8ed0ade17f3738a3eadeacfec7b660a
Manuja Wickramasinghe <manujawicky@gmail.com>
2019-10-15 20:49:18 +0530

    Added a new Binary Search code

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,24 +1,33 @@
 
-public class BinarySearch {
+class BinarySearch { 
 
-	static boolean bsearch(int[] arr, int x) {
-		int low = 0;
-		int high = arr.length-1;
-		while(low <= high) {
-			int mid = (low+high)/2;
-			if(arr[mid] == x)
-				return true;
-			if(arr[mid] < x)
-				low = mid+1;
-			else
-				high = mid-1;
-		}
-		return false;
-	}
+	int binarySearch(int arr[], int l, int r, int x) 
+	{ 
+		if (r >= l) { 
+			int mid = l + (r - l) / 2; 
+			if (arr[mid] == x) 
+				return mid; 
 
-	public static void main(String[] args) {
-		int arr[] = {1, 22, 33, 44, 55, 66, 77, 88, 99, 111};
-		boolean found = bsearch(arr, 2);
-		System.out.println(found);
-	}
-}
+			if (arr[mid] > x) 
+				return binarySearch(arr, l, mid - 1, x); 
+
+			return binarySearch(arr, mid + 1, r, x); 
+		} 
+		return -1; 
+	} 
+
+	public static void main(String args[]) 
+	{ 
+		BinarySearch ob = new BinarySearch(); 
+		int arr[] = { 2, 3, 4, 10, 40 }; 
+		int n = arr.length; 
+		int x = 10; 
+		int result = ob.binarySearch(arr, 0, n - 1, x); 
+		if (result == -1) 
+			System.out.println("Element not present"); 
+		else
+			System.out.println("Element found at index " + result); 
+	} 
+} 
+
+
